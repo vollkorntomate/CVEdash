a2ac9d7d67be3a6078b3000c93670057db069e0c
vollkorntomate <thomas@vollkorntomate.de>
2023-02-02 19:40:32 +0100

    Improve CSS

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -27,10 +27,10 @@
 	<title>CVE Dashboard</title>
 </svelte:head>
 
-<div class="container sm:mx-auto py-2">
-	<div class="flex flex-row flex-wrap md:flex-nowrap">
-		<div class="md:basis-1/2 lg:basis-2/5 order-2 md:order-1 m-2">
-			<div class="dashboard-card max-h-screen-padded overflow-y-scroll no-scrollbar">
+<div class="container sm:mx-auto h-full py-4">
+	<div class="flex flex-row flex-wrap md:flex-nowrap h-full gap-4">
+		<div class="md:basis-1/2 lg:basis-2/5 order-2 md:order-1 h-full">
+			<div class="dashboard-card overflow-y-scroll no-scrollbar h-full">
 				<div class="dashboard-card-title">
 					<span>Latest published CVEs</span>
 				</div>
@@ -59,7 +59,7 @@
 			</div>
 		</div>
 
-		<div class="md:basis-1/2 lg:basis-3/5 w-full order-1 md:order-2 m-2">
+		<div class="md:basis-1/2 lg:basis-3/5 w-full order-1 md:order-2">
 			<div class="dashboard-card">
 				<div class="dashboard-card-title">
 					<span>CVSS Score Statistics</span>
